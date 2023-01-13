809e2103a996c54fa712ecd092dd2a8883cca52b
vidhya1706 <vidhya.b@myglamm.com>
2023-01-13 19:25:49 +0530

    config changes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "",
+    publicPath: "/container/latest/",
   },
   plugins: [
     // new ModuleFederationPlugin({
