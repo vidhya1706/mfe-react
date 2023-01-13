f5319e200bea77262e3f9ead78cd88bc03a64fdb
vidhya1706 <vidhya.b@myglamm.com>
2023-01-13 19:36:34 +0530

    deployment testing

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,8 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/",
+    publicPath: "",
+    // /container/latest/
   },
   plugins: [
     // new ModuleFederationPlugin({
