e195e0a4b9163c8a183910d483f03da496976b4c
vidhya1706 <vidhya.b@myglamm.com>
2023-01-14 19:49:32 +0530

    added comment in marketing file

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+console.log("Hello from marketing app");
 
 export default () => {
   return (
