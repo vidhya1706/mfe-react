dea5cb41ffbbeb1352006bcbac2000bf05705427
vidhya1706 <vidhya.b@myglamm.com>
2023-01-14 19:19:22 +0530

    deployment testing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 console.log("Hello world");
+console.log("Hello world");
+
 export default () => {
   return (
     <div>
