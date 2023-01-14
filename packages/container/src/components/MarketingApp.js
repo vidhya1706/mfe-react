6730d3590ac617cdde876df154dd711c4835c6e5
vidhya1706 <vidhya.b@myglamm.com>
2023-01-14 19:48:52 +0530

    reverted commented code

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,13 @@
-// import { mount } from 'marketing/MarketingApp';
+import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
 
 export default () => {
   const ref = useRef(null);
 
   console.log("hello");
-  // useEffect(() => {
-  //   mount(ref.current);
-  // });
+  useEffect(() => {
+    mount(ref.current);
+  });
 
   return <div ref={ref} />;
 };
